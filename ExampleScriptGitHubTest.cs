f9a5b92c226ac704cd23b06b45527d5b6a7ef2c4
Luke J Turrall-Hendy <lukejameshendy@gmail.com>
2024-11-25 13:56:05 +0000

    Add files via upload

---
+++ ExampleScriptGitHubTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScriptGitHubTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello GitHub");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
