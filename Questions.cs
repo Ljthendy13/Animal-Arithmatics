bbd0a272f469b081059751a83ead73b0b3fb0ec7
Luke J Turrall-Hendy <lukejameshendy@gmail.com>
2024-11-26 11:33:10 +0000

    Added functionality to Questions.cs

--- Questions.cs
+++ Questions.cs
@@ -50,8 +50,208 @@ public class Questions : MonoBehaviour
     void Update()
     {
         if (questionUp == false)
+        {
+            int currentQuestion = Random.Range(0,10);
+            if (currentQuestion == 0)
             {
-                int currentQuestion = new random.randint(0,9);
+                if (answeredOne == false)
+                {
+                    questionUp = true;
+                }
             }
+            if (currentQuestion == 1)
+            {
+                if (answeredTwo == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 2)
+            {
+                if (answeredThree == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 3)
+            {
+                if (answeredFour == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 4)
+            {
+                if (answeredFive == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 5)
+            {
+                if (answeredSix == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 6)
+            {
+                if (answeredSeven == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 7)
+            {
+                if (answeredEight == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 8)
+            {
+                if (answeredNine == false)
+                {
+                    questionUp = true;
+                }
+            }
+            if (currentQuestion == 9)
+            {
+                if (answeredTen == false)
+                {
+                    questionUp = true;
+                }
+            }
+        }
+    }
+
+    public void AnsweredOne()
+    {
+        if (currentQuestion == 0)
+        {
+            answeredOne = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredTwo()
+    {
+        if (currentQuestion == 1)
+        {
+            answeredTwo = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredThree()
+    {
+        if (currentQuestion == 2)
+        {
+            answeredThree = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredFour()
+    {
+        if (currentQuestion == 3)
+        {
+            answeredFour = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredFive()
+    {
+        if (currentQuestion == 4)
+        {
+            answeredFive = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredSix()
+    {
+        if (currentQuestion == 5)
+        {
+            answeredSix = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredSeven()
+    {
+        if (currentQuestion == 6)
+        {
+            answeredSeven = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredEight()
+    {
+        if (currentQuestion == 8)
+        {
+            answeredEight = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredNine()
+    {
+        if (currentQuestion == 8)
+        {
+            answeredNine = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
+    }
+
+    public void AnsweredTen()
+    {
+        if (currentQuestion == 9)
+        {
+            answeredTen = true;
+            questionUp = false;
+        }
+        else
+        {
+            Debug.Log("Wrong Answer.");
+        }
     }
 }
