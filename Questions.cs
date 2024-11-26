43ebb0f0f40e438ec07a3e8f24bbde46bddb27cb
Luke J Turrall-Hendy <lukejameshendy@gmail.com>
2024-11-26 09:10:05 +0000

    Update Questions.cs
    Added some variables

--- Questions.cs
+++ Questions.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Questions : MonoBehaviour
 {
+    
+    public string questionOne = "Question One";
+    public string questionTwo = "Question Two";
+    public string questionThree = "Question Three";
+    public string questionFour = "Question Four";
+    public string questionFive = "Question Five";
+    public string questionSix = "Question Six";
+    public string questionSeven = "Question Seven";
+    public string questionEight = "Question Eight";
+    public string questionNine = "Question Nine";
+    public string questionTen = "Question Ten";
+    
+    public string answerOne = "Answer One";
+    public string answerTwo = "Answer Two";
+    public string answerThree = "Answer Three";
+    public string answerFour = "Answer Four";
+    public string answerFive = "Answer Five";
+    public string answerSix = "Answer Six";
+    public string answerSeven = "Answer Seven";
+    public string answerEight = "Answer Eight";
+    public string answerNine = "Answer Nine";
+    public string answerTen = "Answer Ten";
+    
     // Start is called before the first frame update
     void Start()
     {
