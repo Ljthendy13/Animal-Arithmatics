2adb9ce1fc14dbe47d42d5828d87207c81f2568a
Luke J Turrall-Hendy <lukejameshendy@gmail.com>
2024-12-03 13:54:12 +0000

    Committing and Pushing other misc changes

--- Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7041d271f152b5111e261fb158558856ed49e172")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
